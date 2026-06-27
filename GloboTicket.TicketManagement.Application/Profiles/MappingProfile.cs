494cbd76447f4f98d885e1034824a4437fdcf6c6
Glyn Davies <glynwdavies@gmail.com>
2026-06-27 07:05:12 +0100

    Added CQRS to project.

---
+++ GloboTicket.TicketManagement.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.Features.Events.Queries.GetEventDetail;
+using GloboTicket.TicketManagement.Application.Features.Events.Queries.GetEventsList;
+using GloboTicket.TicketManagement.Domain.Entities;
+
+namespace GloboTicket.TicketManagement.Application.Profiles;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Event, EventListVm>().ReverseMap();
+
+        CreateMap<Event, EventDetailVm>().ReverseMap();
+
+        CreateMap<Category, CategoryDto>().ReverseMap();
+    }
+}
