cb1f48b1e69f5413f12f3a5fb73f3ec4870c9f4a
Glyn Davies <glynwdavies@gmail.com>
2026-06-27 18:43:04 +0100

    Create base repository.

---
+++ GloboTicket.TicketManagement.Persistence/Persistence/BaseRepository.cs
@@ -0,0 +1,54 @@
+﻿using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace GloboTicket.TicketManagement.Persistence.Persistence;
+
+public class BaseRepository<T> : IAsyncRepository<T> where T : class
+{
+    protected readonly GloboTicketDbContext _dbContext;
+
+    public BaseRepository(GloboTicketDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<T> AddAsync(T entity)
+    {
+        await _dbContext.Set<T>().AddAsync(entity);
+
+        await _dbContext.SaveChangesAsync();
+
+        return entity;
+    }
+
+    public virtual async Task<T> GetByIdAsync(Guid id)
+    {
+        var entity = await _dbContext.Set<T>().FindAsync(id);
+
+        if (entity == null)
+        {
+            return null!;
+        }
+
+        return entity;
+    }
+
+    public async Task<IReadOnlyList<T>> ListAllAsync()
+    {
+        return await _dbContext.Set<T>().ToListAsync();
+    }
+
+    public async Task UpdateAsync(T entity)
+    {
+        _dbContext.Entry(entity).State = EntityState.Modified;
+
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(T entity)
+    {
+        _dbContext.Set<T>().Remove(entity);
+
+        await _dbContext.SaveChangesAsync();
+    }
+}
