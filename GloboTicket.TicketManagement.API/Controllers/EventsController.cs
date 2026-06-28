646b8661c1a41bc929922bdd90c31ed6cf42cd9d
Glyn Davies <glynwdavies@gmail.com>
2026-06-28 10:02:38 +0100

    Removed secrets from configuration.

---
+++ GloboTicket.TicketManagement.API/Controllers/EventsController.cs
@@ -0,0 +1,27 @@
+﻿using GloboTicket.TicketManagement.Application.Features.Events.Queries.GetEventsList;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GloboTicket.TicketManagement.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class EventsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public EventsController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet(Name = "GetAllEvents")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesDefaultResponseType]
+    public async Task<ActionResult<List<EventListVm>>> GetAllEvents()
+    {
+        var result = await _mediator.Send(new GetEventsListQuery());
+
+        return Ok(result);
+    }
+}
